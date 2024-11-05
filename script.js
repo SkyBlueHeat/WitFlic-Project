4833d9f47e570f2053334831765bcda00e82ba89
SkyBlueHeat <br.aydin@hotmail.com>
2024-11-06 01:25:55 +0300

    Responsive

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-function toggleMenu() {
-    const menu = document.querySelector('.responsive');
-    menu.classList.toggle('show');
-}
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+document.querySelectorAl(".nav-link").forEach(n => n.
+    addEventListener("click", ()=> {
+        hamburger.classList.remove("active")
+        navMenu.classList.remove("active")
+    }))
+
+
+    // Sınıf adı ile butonları seç
+document.querySelector('.hamburger')
+.addEventListener('click', function() {
+    // Body'ye scroll-down sınıfını ekle veya kaldır
+    document.body.style.transform('translateY(-100px)');
+});
