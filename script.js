69fbf40e2cea72af1b6b8af3c3ecb285c281cc33
SkyBlueHeat <br.aydin@hotmail.com>
2024-11-06 13:31:49 +0300

    Code fix

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 })
-document.querySelectorAl(".nav-link").forEach(n => n.
+document.querySelectorAll(".nav-link").forEach(n => n.
     addEventListener("click", ()=> {
         hamburger.classList.remove("active")
         navMenu.classList.remove("active")
     }))
 
 
-    // Sınıf adı ile butonları seç
-document.querySelector('.hamburger')
-.addEventListener('click', function() {
-    // Body'ye scroll-down sınıfını ekle veya kaldır
-    document.body.style.transform('translateY(-100px)');
-});
+     // Sınıf adı ile butonları seç
+ document.querySelector('.hamburger')
+ .addEventListener('click', function() {
+     // Body'ye scroll-down sınıfını ekle veya kaldır
+     document.body.style.transform('translateY(-100px)');
+ });
